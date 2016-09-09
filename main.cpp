98709a922d47d595fd15309666ad172577c5d0ed
Gopika Pai <gopikaspai@users.noreply.github.com>
2016-09-09 21:07:35 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,137 @@
+/*
+ * GLUT Shapes Demo
+ *
+ * Written by Nigel Stewart November 2003
+ *
+ * This program is test harness for the sphere, cone
+ * and torus shapes in GLUT.
+ *
+ * Spinning wireframe and smooth shaded shapes are
+ * displayed until the ESC or q key is pressed.  The
+ * number of geometry stacks and slices can be adjusted
+ * using the + and - keys.
+ */
+#include<windows.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+
+
+#include<GL/gl.h>
+#include<GL/glu.h>
+#include<GL/glut.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
+#define dx 15
+#define dy 15
+#define max_x 20
+#define max_y 25
+
+#include<GL/glut.h>
+#include<GL/glu.h>
+#include<GL/gl.h>
+
+GLfloat vertices[][3]={{-0.25,-0.25,-0.25},{-0.25,0.25,-0.25},{0.25,0.25,-0.25},{0.25,-0.25,-0.25},{-0.25,-0.25,0.25},{-0.25,0.25,0.25},{0.25,0.25,0.25},{0.25,-0.25,0.25}};
+GLfloat colors[][3]={{0.0,0.0,0.0},{0.0,1.0,0.0},{0.0,1.0,1.0},{0.0,0.0,1.0},{1.0,0.0,0.0},{1.0,1.0,0.0},{1.0,1.0,1.0},{1.0,0.0,1.0}};
+
+void Draw_Rect(int xmin,int ymin,int xmax,int ymax)
+ {
+  glBegin(GL_LINE_LOOP);
+  glColor3f(1,1,1);
+  glVertex2d(xmin,ymin);
+  glVertex2d(xmax,ymin);
+  glVertex2d(xmax,ymax);
+  glVertex2d(xmin,ymax);
+  glEnd();
+ }
+void polygon(int a,int b,int c,int d)
+{
+	glBegin(GL_POLYGON);
+	glColor3fv(colors[a]);
+	glVertex3fv(vertices[a]);
+	glColor3fv(colors[b]);
+	glVertex3fv(vertices[b]);
+	glColor3fv(colors[c]);
+	glVertex3fv(vertices[c]);
+	glColor3fv(colors[d]);
+	glVertex3fv(vertices[d]);
+	glEnd();
+}
+void colorcube(void)
+{
+	polygon(0,3,2,1);
+	polygon(2,3,5,6);
+	polygon(0,4,7,3);
+	polygon(1,2,6,5);
+	polygon(4,5,6,7);
+	polygon(0,1,5,4);
+}
+static GLfloat theta[]={0.0,0.0,0.0};
+static GLint axis=2;
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glRotatef(theta[0],1.0,0.0,0.0);
+	glRotatef(theta[1],0.0,1.0,0.0);
+	glRotatef(theta[2],0.0,0.0,1.0);
+
+	colorcube();
+
+	Draw_Rect(0.45,0.5,1.25,1.5);
+	glFlush();
+
+	glutSwapBuffers();
+}
+void spinCube()
+{
+	theta[axis]+=1.0;
+	if(theta[axis]>360.0)
+	  theta[axis]-=360.0;
+	glutPostRedisplay();
+}
+void mouse(int btn,int state,int x,int y)
+{
+	if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
+	  axis=0;
+	else if(btn==GLUT_MIDDLE_BUTTON && state==GLUT_DOWN)
+		axis=1;
+	     else if(btn==GLUT_RIGHT_BUTTON&&state==GLUT_DOWN)
+		     axis=2;
+}
+void myReshape(int w,int h)
+{
+	glViewport(0,0,w,h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if(w<=h)
+		glOrtho(-2.0,2.0,-2.0*(GLfloat)h/(GLfloat)w,2.0*(GLfloat)h/(GLfloat)w,-10.0,10.0);
+	else
+		glOrtho(-2.0*(GLfloat)w/(GLfloat)h,2.0*(GLfloat)w/(GLfloat)h,-2.0,2.0,-10.0,10.0);
+	glMatrixMode(GL_MODELVIEW);
+
+}
+
+int main(int argc,char **argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
+	glutInitWindowSize(800,800);
+	glutCreateWindow("Spinning cube");
+	glutReshapeFunc(myReshape);
+	glutDisplayFunc(display);
+	glutIdleFunc(spinCube);
+	glutMouseFunc(mouse);
+	glEnable(GL_DEPTH_TEST);
+	glutMainLoop();
+	return 0;
+}
